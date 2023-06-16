4ce26b5ff1be3ee9e7526f1bef4443496960df6c
Hanis <67252046+hanisdhuzuki@users.noreply.github.com>
2023-06-16 20:58:54 +0800

    Update minechain_charity.sol

--- minechain_charity.sol
+++ minechain_charity.sol
@@ -2,51 +2,70 @@
 pragma solidity ^0.8.0;
 
 contract Charity {
-    // Define the owner of this contract
     address public owner;
-    // Define the threshold value for transactions
     uint public threshold = 10 ether;
-    // Define the current balance of the contract
     uint public balance = 0;
-    // Define an event to log all transactions
+    mapping(address => bool) public isDonor;
+    mapping(address => bool) public isAlerted;
+
     event LogTransaction(address indexed sender, address indexed recipient, uint amount);
-    // Modifier to check if the sender is the owner
+    event Alert(address indexed account, uint amount, string alertType);
+
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Only the owner can call this function.");
         _;
     }
-    // Constructor that sets the initial owner of the contract
+
     constructor() {
         owner = msg.sender;
         balance = 0;
     }
-    // Function to set the threshold value
+
     function setThreshold(uint newThreshold) public onlyOwner {
         threshold = newThreshold;
     }
-    // Function to donate funds into the contract
+
     function donate(uint amount) public payable {
-        require(amount > 0);
+        require(amount > 0, "Donation amount must be greater than zero.");
+        require(msg.value == amount, "Please send the exact donation amount.");
+
         balance += amount;
+        isDonor[msg.sender] = true;
+
+        if (amount > threshold) {
+            emit Alert(msg.sender, amount, "HugeTransaction");
+        }
+
+        if (address(this).balance > 50 ether && !isAlerted[msg.sender]) {
+            emit Alert(msg.sender, balance, "MoneyLaundering");
+            isAlerted[msg.sender] = true;
+        }
+
         emit LogTransaction(msg.sender, address(0), amount);
     }
-    // Function to withdraw funds from the contract
+
     function withdraw(uint amount) public {
-        require(amount > 0);
-        require(balance >= amount);
+        require(amount > 0, "Withdrawal amount must be greater than zero.");
+        require(balance >= amount, "Insufficient balance.");
+
         balance -= amount;
         payable(msg.sender).transfer(amount);
         emit LogTransaction(msg.sender, address(0), amount);
+
+        if (address(this).balance > 50 ether && !isAlerted[msg.sender]) {
+            emit Alert(msg.sender, balance, "MoneyLaundering");
+            isAlerted[msg.sender] = true;
+        }
     }
-    // Function to check if the contract balance is above the threshold
+
     function isAboveThreshold() public view returns (bool) {
         return balance > threshold;
     }
-    // Function to get the current balance of the contract
+
     function getBalance() public view returns (uint) {
         return balance;
     }
-    // Function to get the owner of the contract
+
     function getOwner() public view returns (address) {
         return owner;
     }
