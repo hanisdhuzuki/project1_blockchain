beb75b977ad720184dff282df5abc16e5ee2e73b
kinah00 <74648495+kinah00@users.noreply.github.com>
2023-07-01 13:02:01 +0800

    Update minechain_charity.sol

--- minechain_charity.sol
+++ minechain_charity.sol
@@ -22,13 +22,13 @@ contract Charity {
     mapping(address => bool) public isThisAddressRegistered;
     mapping(uint => Donatee) private donatees;
     mapping(uint => Donator) private donators;
-    address[] private walletAddressDonatee;
+    address[] private fullNameDonatee;
 
     event LogTransaction(address indexed sender, address indexed recipient, uint amount);
     event Alert(address indexed account, uint amount, string alertType);
 
     modifier onlyOwner() {
-        require(0xFD0153e404343462A05D38989BAae635a9194078 == owner, "Only the owner can call this function.");
+        require(0xFD0153e404343462A05D38989BAae635a9194078 == owner, "Only the owner can call this function."); //owner wallet address
         _;
     }
 
@@ -44,62 +44,49 @@ contract Charity {
     function registerAsDonatee(address _donateeAddress, uint _last4digitDonatee, string memory _fullName) public {
         require(!isThisAddressRegistered[_donateeAddress], "You have been registered");
 
-        isThisAddressRegistered[msg.sender] = true;
+        isThisAddressRegistered[_donateeAddress] = true;
         donatees[_last4digitDonatee] = Donatee(_donateeAddress, _last4digitDonatee, _fullName);
-        walletAddressDonatee.push(_donateeAddress);
-        /**donatees.push(Donatee({
-            donateeAddress: msg.sender,
-            donateeName: donateeName
-        }));*/
+        walletAddressDonatee.push(_fullName);
     }
 
     function registerAsDonor(address _donatorAddress, uint _last4digitDonator) public {
         require(!isThisAddressRegistered[_donatorAddress], "You have been registered");
 
-        isThisAddressRegistered[msg.sender] = true;
+        isThisAddressRegistered[_donatorAddress] = true;
         donators[_last4digitDonator] = Donator(_donatorAddress, _last4digitDonator);
-        walletAddressDonatee.push(_donatorAddress);
-        /**donatees.push(Donatee({
-            donateeAddress: msg.sender,
-            donateeName: donateeName
-        }));*/
+        
     }
 
-    function donate(uint amount) public payable {
+    function donate(uint amount, address payable _donatorAddress) public payable {
         require(amount > 0, "Donation amount must be greater than zero.");
-        //require(msg.value == amount, "Please send the exact donation amount.");
 
         balance += amount;
         isDonor[msg.sender] = true;
 
         if (amount > threshold) {
-            emit Alert(msg.sender, amount, "HugeTransaction");
+            emit Alert(_donatorAddress, amount, "HugeTransaction");
         }
 
         if (balance > 50 ether) {
-            emit Alert(msg.sender, balance, "MoneyLaundering");
+            emit Alert(_donatorAddress, balance, "MoneyLaundering");
             isAlerted[msg.sender] = true;
         }
 
-        emit LogTransaction(msg.sender, address(0), amount);
+        emit LogTransaction(_donatorAddress, address(0), amount);
     }
 
-    function withdraw(uint amount) public {
+    function withdraw(uint amount, address _donateeAddress) public {
         require(amount > 0, "Withdrawal amount must be greater than zero.");
         require(balance >= amount, "Insufficient balance.");
 
         balance -= amount;
-        payable(msg.sender).transfer(amount);
-        emit LogTransaction(msg.sender, address(0), amount);
+        payable(_donateeAddress).transfer(amount);
+        emit LogTransaction(_donateeAddress, address(0), amount);
 
         if (balance > 50 ether) {
-            emit Alert(msg.sender, balance, "MoneyLaundering");
-            isAlerted[msg.sender] = true;
+            emit Alert(_donateeAddress, balance, "MoneyLaundering");
+            isAlerted[_donateeAddress] = true;
         }
-    }
-
-    function isAboveThreshold() public view returns (bool) {
-        return balance > threshold;
     }
 
     function getBalance() public view returns (uint) {
@@ -113,6 +100,6 @@ contract Charity {
     //fx to list all need donate
     function list() public view returns (address[] memory) {
         //return the array of the wallet address
-        return walletAddressDonatee;
+        return fullNameDonatee;
     }
 }
